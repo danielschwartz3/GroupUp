789b6741e2438661b3e727ec4984ec9fb180fc01
Edem Nuviadenu <edemnuviadenu@gmail.com>
2021-02-03 14:01:38 -0500

    Add registration validations

---
+++ GroupUp-Backend/src/main/java/ca/mcgill/ecse428/groupup/model/EmailDomain.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse428.groupup.model;
+
+public enum EmailDomain {
+    MCGILL("mail.mcgill.ca"),
+    CARLETON("carleton.ca");
+
+    String domain;
+
+    EmailDomain(String domain){
+        this.domain = domain;
+    }
+
+    public String getDomain(){return domain;}
+}
--- GroupUp-Backend/src/main/java/ca/mcgill/ecse428/groupup/service/AccountService.java
+++ GroupUp-Backend/src/main/java/ca/mcgill/ecse428/groupup/service/AccountService.java
@@ -14,8 +14,29 @@ public class AccountService {
 
     public Account  createAccount(UserRole role, String username, String student_name,
                                  String user_email, String user_institution, String password)
-                                 {                         
-        Account acc = new Account();
+                                 {   
+        Account acc;
+        String error = "";
+        Boolean isEmailValid = false;
+        if(role == null) error += "User's role cannot be empty";
+        if(username == null || username.trim().length()==0) error += "Username cannot be empty";
+        if(student_name == null || student_name.trim().length()==0) error += "User's full-name cannot be empty";
+        if(user_email == null || user_email.trim().length()==0) error += "User's email cannot be empty";
+        if(user_institution == null ||user_institution.trim().length()==0) error += "User institution cannot be empty";
+        if(password == null || password.trim().length()==0) error += "Password cannot be empty";
+        
+        //verify email
+        int posOfdomain = user_email.indexOf('@');
+        String domain = user_email.substring(posOfdomain);
+        for (EmailDomain d: EmailDomain.values()){
+            if(d.getDomain().equals(domain)){
+                isEmailValid = true;
+            }
+        }
+        if(!isEmailValid) error+= "INVALID_EMAIL";
+        if(accRepo.existsById(username))error = error + "Already registered";
+
+        acc = new Account();
         acc.setUsername(username);
         acc.setFullName(student_name);
         acc.setEmail(user_email);
