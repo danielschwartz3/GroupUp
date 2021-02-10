8bfddcf4c8927363469d9d7a8f10ed0c68dbed42
Edem Nuviadenu <edemnuviadenu@gmail.com>
2021-02-10 16:41:07 -0500

    Add controller to register user

--- GroupUp-Backend/src/main/java/ca/mcgill/ecse428/groupup/controller/AccountController.java
+++ GroupUp-Backend/src/main/java/ca/mcgill/ecse428/groupup/controller/AccountController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse428.groupup.model.Account;
+import ca.mcgill.ecse428.groupup.model.Admin;
 import ca.mcgill.ecse428.groupup.model.Student;
 import ca.mcgill.ecse428.groupup.service.AccountService;
 
@@ -15,14 +16,25 @@ public class AccountController{
     @Autowired
     private AccountService accountService;
 
-    @PostMapping(value ={"/register/newstudent","/register/newstudent/"})
+    @PostMapping(value ={"/register/newStudent","/register/newStudent/"})
     public Account createStudentAccount (@RequestParam("userName")String userName,
                                         @RequestParam("name")String name,
                                         @RequestParam("email")String email,
                                         @RequestParam("institution")String institution,
                                         @RequestParam("password")String password)
                                         throws IllegalArgumentException{
-     Account acc = accountService.createAccount(new Student(), userName, name, email, institution, password);                                  
+     Account acc = accountService.createStudentAccount(new Student(), userName, name, email, institution, password);                                  
+     return acc;
+    }
+
+    @PostMapping(value ={"/register/newAdmin","/register/newAdmin/"})
+    public Account createAdminAccount (@RequestParam("userName")String userName,
+                                        @RequestParam("name")String name,
+                                        @RequestParam("email")String email,
+                                        @RequestParam("institution")String institution,
+                                        @RequestParam("password")String password)
+                                        throws IllegalArgumentException{
+     Account acc = accountService.createAdminAccount(new Admin(), userName, name, email, institution, password);                                  
      return acc;
     }
   
--- GroupUp-Backend/src/main/java/ca/mcgill/ecse428/groupup/service/AccountService.java
+++ GroupUp-Backend/src/main/java/ca/mcgill/ecse428/groupup/service/AccountService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ca.mcgill.ecse428.groupup.dao.AccountRepository;
+import ca.mcgill.ecse428.groupup.dao.AdminRepository;
+import ca.mcgill.ecse428.groupup.dao.StudentRepository;
 import ca.mcgill.ecse428.groupup.model.*;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,32 +15,25 @@ public class AccountService {
     
     @Autowired
     AccountRepository accRepo;
+    @Autowired
+    StudentRepository stuRepo;
+    @Autowired
+    AdminRepository adminRepo;
 
     @Transactional
-    public Account  createAccount(UserRole role, String userName, String name,
+    public Account  createStudentAccount(Student role, String userName, String name,
                                  String email, String institution, String password)
                                  {   
         Account acc;
         String error = "";
         Boolean isEmailValid = false;
-        if(role == null) error += "User's role cannot be empty";
-        if(userName == null || userName.trim().length()==0) error += "Username cannot be empty";
-        if(name == null || name.trim().length()==0) error += "User's full-name cannot be empty";
-        if(email == null || email.trim().length()==0) error += "User's email cannot be empty";
-        if(institution == null ||institution.trim().length()==0) error += "User institution cannot be empty";
-        if(password == null || password.trim().length()==0) error += "Password cannot be empty";
-        
-        //verify email
-        int posOfdomain = email.indexOf('@');
-        String domain = email.substring(posOfdomain);
-        for (EmailDomain d: EmailDomain.values()){
-            if(d.getDomain().equals(domain)){
-                isEmailValid = true;
-            }
-        }
+        error = verifyInput(role, userName, name, email, institution, password);
+        isEmailValid = verifyEmail(email);
         if(!isEmailValid) error+= "INVALID_EMAIL";
         if(accRepo.existsById(userName))error = error + "Already registered";
-
+        if (error.length() > 0) {
+            throw new IllegalArgumentException(error);
+        }
         acc = new Account();
         acc.setUsername(userName);
         acc.setFullName(name);
@@ -46,23 +41,79 @@ public class AccountService {
         acc.setInstitution(institution);
         acc.setPassword(password);
         acc.setUserRole(role);
-
+        role.setAccount(acc);
+        stuRepo.save(role);
         accRepo.save(acc);
-
+        return acc;
+    }
+    @Transactional
+    public Account  createAdminAccount(Admin role, String userName, String name,
+                                 String email, String institution, String password)
+                                 {   
+        Account acc;
+        String error = "";
+        Boolean isEmailValid = false;
+        error = verifyInput(role, userName, name, email, institution, password);
+        isEmailValid = verifyEmail(email);
+        if(!isEmailValid) error+= "INVALID_EMAIL";
+        if(accRepo.existsById(userName))error = error + "Already registered";
+        if (error.length() > 0) {
+            throw new IllegalArgumentException(error);
+        }
+        acc = new Account();
+        acc.setUsername(userName);
+        acc.setFullName(name);
+        acc.setEmail(email);
+        acc.setInstitution(institution);
+        acc.setPassword(password);
+        acc.setUserRole(role);
+        role.setAccount(acc);
+        adminRepo.save(role);
+        accRepo.save(acc);
         return acc;
     }
 
+
+    
     @Transactional
     public Account LogIn(String email, String password) throws IllegalArgumentException{
         Account acc = accRepo.findByEmail(email);
         if(acc == null) {
 			throw new IllegalArgumentException("Username cannot be found.");
 		} else if(!acc.getPassword().equals(password)) {
-			throw new IllegalArgumentException("Password is incorrect.");
+            throw new IllegalArgumentException("Password is incorrect.");
 		}
         return acc;
     }
+    
+    
+   /**
+    * Helper methods for service class
+    * @param email
+    * @return
+    */ 
+    private Boolean verifyEmail(String email) {
+        Boolean isValid = false;
+        int offset = 1;
+        int posOfdomain = email.indexOf('@');
+        String domain = email.substring(posOfdomain + offset);
+        for (EmailDomain d: EmailDomain.values()){
+            if(d.getDomain().equals(domain)){
+                isValid = true;
+            }
+        }
+        return isValid;
+    }
 
-
-
+    private String verifyInput(UserRole role, String userName, String name, String email, String institution,
+            String password) {
+            String error = "";
+            if(role == null) error += "User's role cannot be empty";
+            if(userName == null || userName.trim().length()==0) error += "Username cannot be empty";
+            if(name == null || name.trim().length()==0) error += "User's full-name cannot be empty";
+            if(email == null || email.trim().length()==0) error += "User's email cannot be empty";
+            if(institution == null ||institution.trim().length()==0) error += "User institution cannot be empty";
+            if(password == null || password.trim().length()==0) error += "Password cannot be empty";
+        return error;
+    }
 }
