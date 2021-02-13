4b6dd0b61124b8df703150c02b14d00430f683c8
gregorywalfish <gregory.walfish@mail.mcgill.ca>
2021-02-12 23:25:45 -0500

    login almost done

--- GroupUp-Frontend/src/Components/Login/Login.js
+++ GroupUp-Frontend/src/Components/Login/Login.js
@@ -1,17 +1,100 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Button, Modal, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
+const URL = 'http://localhost:8080'
 
-class Login extends Component {
+const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-
-    render() {
-        return (
-            <div>
-                <div>hello</div>
-                <div>hello</div>
-            </div>
-        )
+    function getModalStyle() {
+        const top = 50;
+        const left = 50;
+      
+        return {
+          top: `${top}%`,
+          left: `${left}%`,
+          transform: `translate(-${top}%, -${left}%)`,
+        };
     }
+    
+    const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: 'absolute',
+        width: 500,
+        backgroundColor: 'white',
+        padding: theme.spacing(2, 4, 3),
+    },
+    }));
+
+    const Login = () => {
+        axios.post(`${URL}/Login`, null, {
+            params: {
+                email: email,
+                password: password,
+            }
+        }).then(function (response) {
+            console.log(response);
+            const newStudent = {
+                
+                email: email,
+                
+                password: password,
+            }
+            // const newCourses = [...props.courses, newCourse];
+            // props.setCourses(newCourses);
+            handleCreateModal();
+            
+            setEmail('');
+           
+            setPassword('');
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+
+    const classes = useStyles();
+
+    const [modalStyle] = React.useState(getModalStyle);
+
+    const [createLoginModal, setCreateLoginModal] = useState(props.data);
+
+    const handleCreateModal = () => {
+        setCreateLoginModal(!createLoginModal);
+    };
+
+    const body = (
+        <div style={modalStyle} className={classes.paper}>
+          <h2 id="simple-modal-title">Login</h2>
+          <p id="simple-modal-description">
+              Please enter your information to login.
+          </p>
+          
+           <TextField onChange={e => setEmail(e.target.value)} style={{ margin: '1%', width: '45%' }} id="email" label="Email" />
+          <TextField onChange={e => setPassword(e.target.value)} style={{ margin: '1%', width: '45%' }} id="password" label="Password" />
+          
+          <Button style={{ width: '100%', marginTop: '5%' }} onClick={Login}>Login</Button>
+        </div>
+    );
+      
+
+    return (
+        <div>
+          <Button onClick={handleCreateModal}>login</Button>
+          <Modal
+            open={Login}
+            onClose={handleCreateModal}
+            aria-labelledby="Login"
+            aria-describedby="Please enter your information to login."
+          >
+            {body}
+          </Modal>
+        </div>
+    );
 }
+
 
 export default Login;
