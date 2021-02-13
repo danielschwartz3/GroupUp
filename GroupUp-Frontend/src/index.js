a1628d3c3ecbfb71b0cecab6ef7ab8b06a7db6c4
ashaduzzamane <enan@live.ca>
2021-02-12 21:44:25 -0500

    fix file structure

--- GroupUp-Frontend/src/index.js
+++ GroupUp-Frontend/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Login from './Components/Login/Login'
-import Navbar from './Components/Navbar/Navbar'
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
@@ -14,9 +12,7 @@ const store = createStore(allReducers)
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={ store }>
-      {/* <App /> */}
-      <Navbar />
-      <Login />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
