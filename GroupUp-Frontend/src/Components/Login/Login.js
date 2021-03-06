235ba77325c76667d991b90b81cf5cea8413aaeb
danielschwartz3 <71156919+danielschwartz3@users.noreply.github.com>
2021-03-05 19:48:35 -0500

    Removing unused code

--- GroupUp-Frontend/src/Components/Login/Login.js
+++ GroupUp-Frontend/src/Components/Login/Login.js
@@ -4,7 +4,6 @@ import { Button, Modal, TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import { intializeUserAction, initializeUserCoursesAction } from '../../redux';
-// import { setCookie, getCookie, eraseCookie}  from '../../cookies';
 import Cookies from 'js-cookie'
 
 const URL = 'http://localhost:8080'
