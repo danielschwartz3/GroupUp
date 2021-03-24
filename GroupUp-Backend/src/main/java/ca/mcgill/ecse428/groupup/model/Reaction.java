132f6af80600068d68e7fe51ecf3ddad35aa3f9e
Edem Nuviadenu <edemnuviadenu@gmail.com>
2021-03-24 11:39:03 -0400

    Add reaction model

---
+++ GroupUp-Backend/src/main/java/ca/mcgill/ecse428/groupup/model/Reaction.java
@@ -0,0 +1,79 @@
+package ca.mcgill.ecse428.groupup.model;
+
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Reaction {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    private ReactionType reactionType;
+
+    @ManyToOne
+    private Message reactionMessage;
+
+    @ManyToOne
+    private Student reactor;
+
+    @Basic
+	@Temporal(TemporalType.TIMESTAMP)
+    private Date reactionDate;
+
+    
+    public long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public ReactionType getReactionType(){
+        return reactionType;
+    }
+
+    public void setReactionType(ReactionType reactionType){
+        this.reactionType = reactionType;
+    }
+
+    public Message getReactionMessage(){
+        return reactionMessage;
+    }
+
+    public void setReactionMessage(Message reactionMessage){
+        this.reactionMessage = reactionMessage;
+    }
+
+    public Student getReactor(){
+        return reactor;
+    }
+
+    public void setReactor(Student reactor){
+        this.reactor = reactor;
+    }
+
+    public Date getReactionDate() {
+		return reactionDate;
+	}
+	
+	public void setReactionDate(Date reactionDate) {
+		this.reactionDate = reactionDate;
+	}
+
+    
+    
+}
